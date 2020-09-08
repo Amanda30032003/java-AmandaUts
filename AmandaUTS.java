7a351042898c9899219f26929dc8f3123d34d79a
Amanda30032003 <70932350+Amanda30032003@users.noreply.github.com>
2020-09-08 19:17:52 +0700

    Add files via upload

---
+++ AmandaUTS.java
@@ -0,0 +1,58 @@
+
+package amandauts;
+
+
+public class AmandaUTS {
+
+    public static void main(String[] args) {
+       String a="ABCDEFGHIJKLMNOPQRSTUPWXYZ#-";
+       char hasil=a.charAt(0);
+       char b=a.charAt(12);
+       char c=a.charAt(13);
+       char d=a.charAt(3);
+       char e=a.charAt(a.length()-1);
+       char f=a.charAt(19);
+       char g=a.charAt(7);
+       char h=a.charAt(24);
+       char i=a.charAt(17);
+       char j=a.charAt(8);
+       char k=a.charAt(15);
+       char l=a.charAt(20);
+       char m=a.charAt(25);
+       
+       System.out.print(hasil);
+       System.out.print(b);
+       System.out.print(hasil);
+       System.out.print(c);
+       System.out.print(d);
+       System.out.print(hasil);
+       System.out.print(e);
+       System.out.print(f);
+       System.out.print(g);
+       System.out.print(h);
+       System.out.print(i);
+       System.out.print(hasil);
+       System.out.print(c);
+       System.out.print(j);
+       System.out.print(e);
+        System.out.print(k);
+         System.out.print(l);
+          System.out.print(f);
+          System.out.print(i);
+           System.out.print(j);
+            System.out.print(e);  
+            System.out.print(hasil);
+             System.out.print(m);
+              System.out.print(m);
+               System.out.print(hasil);
+                System.out.print(g);
+                 System.out.print(i);
+                  System.out.print(hasil);
+       
+       
+       
+       
+       
+    }
+    
+}
